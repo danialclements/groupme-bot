d79082c8dde365586ad681fbf99761ebaecc8367
Meegul304 <micha3lbilstein@gmail.com>
2016-12-07 18:34:43 -0500

    WORK PLEASE

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const bot = {
     bot_ID: "",
     group_ID: "",
     sendMessage(text) {
-        const toSend = `${this.api_url}post?bot_id=${this.bot_ID}&text=${encodeURIComponent(text)}`;
+        const toSend = `${bot.api_url}post?bot_id=${bot.bot_ID}&text=${encodeURIComponent(text)}`;
         request.post(toSend, (error, response, body) => {
             if (error) {
                 console.log(error);
